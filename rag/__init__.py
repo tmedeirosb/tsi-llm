ad36fd09c47e5f063076f5e9e2d12e84f45cfc9d
Thiago Medeiros Barros <thiago.mdeiros@gmail.com>
2025-06-08 21:27:20 -0300

    Add YouTube transcript and summary agents (#2)

--- rag/__init__.py
+++ rag/__init__.py
@@ -2,12 +2,10 @@
 """
 Pacote RAG
 ----------
-Arquivo de inicialização do pacote 'rag'. 
-Deixa explícito que a pasta 'rag' é um módulo Python.
+Arquivo de inicialização do pacote 'rag'.
 """
 
-# Aqui você pode importar, se quiser, as funções mais importantes diretamente
-# para o namespace do pacote. Por exemplo:
-# from .ingestion import ingest_data, clear_db
-# from .retrieval import get_relevant_docs
-# from .generation import generate_answer
+from .ingestion import ingest_data, clear_db
+from .retrieval import load_db, get_relevant_docs
+from .generation import generate_answer
+from .youtube_agents import TranscriptAgent, SummaryQAAgent
